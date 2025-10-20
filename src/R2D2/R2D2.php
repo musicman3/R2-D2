4a4e97c2dcc33d9e342edc3e34415f7b670c9d3c
LC <LC@DESKTOP-P5F17PB.Dlink>
2025-10-20 16:15:05 +0300

    Init file

---
+++ src/R2D2/R2D2.php
@@ -0,0 +1,23 @@
+<?php
+
+/* =-=-=-= Copyright © 2025 R2D2 =-=-=-=-=-= 
+  |           APACHE-2.0 LICENSE            |
+  |   https://github.com/musicman3/Cruder   |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+declare(strict_types=1);
+
+namespace R2D2;
+
+/**
+ * R2D2 PHP Router
+ *
+ * @package R2D2 
+ * @author R2D2 Team
+ * @copyright © 2025 R2D2
+ * @license Apache-2.0
+ * 
+ */
+class R2D2 {
+    
+}
